cc1470c7d5d49607f4452923f01f456747d581da
dustyRose12 <dustyrose125@gmail.com>
2017-08-21 21:10:41 -0500

    store item exercise

--- store_item.rb
+++ store_item.rb
@@ -3,6 +3,6 @@ item_2 = {item_name: "necklace", color: "rose", metal: "gold", price: 80 }
 item_3 = {:item_name => "bracelet", :color => "silver", :metal => "platinum", price: 100 }
 
 puts "The #{item_1[:color]} #{item_1[:metal]} #{item_1[:item_name]} cost #{item_1[:price]} dollars."
-puts "The #{item_2[:color]} #{item_2[:metal]} #{item_2[:item_name]} cost #{item_2[:price]} dollars."
-puts "The #{item_3[:color]} #{item_3[:metal]} #{item_3[:item_name]} cost #{item_3[:price]} dollars."
+puts "The #{item_2[:color]} #{item_2[:metal]} #{item_2[:item_name]} costs #{item_2[:price]} dollars."
+puts "The #{item_3[:color]} #{item_3[:metal]} #{item_3[:item_name]} costs #{item_3[:price]} dollars."
 
